ed78fabfaabc010c2b1b38f3efae983643e2fb85
allsaidX <allsaidx7@gmail.com>
2020-03-01 17:14:56 +0300

    upload files

---
+++ EsoftCorporation/Esoft_Corporation/Esoft_Corporation/Source/LibDll/ElementLib/WareHouse/clothClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esoft_Corporation.Source.LibDll.ElementLib.WareHouse
+{
+    class clothClass
+    {
+    }
+}
---
+++ EsoftCorporation/Esoft_Corporation/Esoft_Corporation/Source/LibDll/uClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Esoft_Corporation.Source.LibDll.ElementLib;
+
+namespace Esoft_Corporation.Source.LibDll
+{
+    class uClass
+    {
+        #region -- Structure --
+
+        private string _email;
+        private string _password;
+        private string _rule;
+        private string _name;
+        private string _surname;
+        
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public string Rule
+        {
+            get { return _rule; }
+            set { _rule = value; }
+        }
+        #endregion
+
+        #region -- Functional --
+
+
+
+        #endregion
+
+        
+    }
+}
